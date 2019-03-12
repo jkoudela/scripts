f5c6ee9c7826bcad873a5c97a1d55fa860047151
jkoudela <jan@koudela.eu>
2019-03-12 21:21:58 +0100

    Added git config and SSH

--- go_environment.sh
+++ go_environment.sh
@@ -2,3 +2,6 @@ export GOPATH="${HOME}/go"
 export GOBIN="${GOPATH}/bin"
 export PATH="${PATH}:${GOBIN}"
 
+git config --global user.name "jkoudela"
+git config --global user.email "jan@koudela.eu"
+ssh-keygen -t rsa -b 4096 -C "jan@koudela.eu"
