4d5a424f22017088aeb859e2817817154b0865d7
jkoudela <jkoudela@users.noreply.github.com>
2019-03-16 20:38:16 +0100

    Echo parameter -e

--- go_environment_ubuntu.sh
+++ go_environment_ubuntu.sh
@@ -13,9 +13,9 @@ mkdir src
 mkdir pkg
 cd $HOME
 
-echo "export GOPATH=\"$HOME/go\"" >> ~/.bash_profile
-echo "export GOBIN=\"$GOPATH/bin\"" >> ~/.bash_profile
-echo "export PATH=\"$PATH:$GOBIN\"" >> ~/.bash_profile
+echo -e "export GOPATH=\"$HOME/go\"" >> ~/.bash_profile
+echo -e "export GOBIN=\"$GOPATH/bin\"" >> ~/.bash_profile
+echo -e "export PATH=\"$PATH:$GOBIN\"" >> ~/.bash_profile
 
 git config --global user.name "jkoudela"
 git config --global user.email "jan@koudela.eu"
